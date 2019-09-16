d05888d4bcc2b29b5d3820dd6ee7daaf759e9402
Varshit1006 <54777494+Varshit1006@users.noreply.github.com>
2019-09-17 00:58:36 +0530

    Update locus.c

--- locus.c
+++ locus.c
@@ -3,8 +3,84 @@
 #include <math.h>
 #include "coeffs.h"
 
-double **meshgrid(int len, int begin, double addX, int addY);
-void saveMat(double** mat, char*str, double m, double n);
+
+
+void saveMat (double **mat , char *str, int m , int n){
+  FILE *fp;
+  fp = fopen(str,"w");
+  for (int i = 0; i < m; i++){
+    for (int j = 0; j < n; j++){
+      fprintf(fp , "%lf",mat[i][j]);
+    }
+    fprintf(fp ,"\n");
+  }
+  fclose(fp);
+}
+//end function
+
+//Function for saving a value to .dat file
+void saveValue(char *str,double a){
+  FILE *fp;
+fp = fopen(str,"w");
+fprintf(fp, "%lf",a );
+fclose(fp);
+}
+//end function
+
+
+//Linspace function
+double **linspace(double a, double l, int m)
+{
+//Variable declarations
+double d, **ap;
+int i,j;
+
+ap = createMat(m,1);
+
+//Common difference
+d = (l-a)/(m-1);
+
+ for(i=0;i<1;i++)
+ {
+  for(j=0;j<m;j++)
+  {
+ap[j][i] = a+j*d;
+
+  }
+ }
+//Returning the address of the first memory block
+return ap;
+}
+//End linspace function
+
+
+double **meshgrid(int len, int begin, double addX, int addY)
+{
+  double ** ret = createMat(len, len);
+  for (int i=0; i<len; i++)
+    for (int j=0; j<len; j++){
+      ret[i][j] = begin + i*addY + j*addX;
+    }
+  return ret;
+}
+
+
+double rootofeqn1(double a,double b,double c){
+    double root1;
+    double det;
+    det=sqrt((b*b)-4*a*c);
+        root1 =(((-b)+det)/(2*a));
+        return root1;
+    }
+
+double rootofeqn2(double a,double b,double c){
+    double root2;
+    double det;
+    det=sqrt((b*b)-4*a*c);
+        root2 =(((-b)-det)/(2*a));
+        return root2;
+    }
+
 
 int main(){
 	int len = 9;
@@ -39,56 +115,31 @@ int main(){
     printf("\n\n");
     saveMat(A, "A.dat", 2, 1);
     
-    double **F = createMat(1,1);
-    F[0][0] = 16 ;
-    print(F,1,1);
-    printf("\n\n");
-    saveMat(F, "F.dat", 1, 1);
+    double F = -16;
+    printf("%f\n",F );
+    saveValue("F.dat",F);
+
+
+    double p = 1;
+    double q = -(V[0][0]+V[1][1])/16;
+    double r = ((V[0][0])*(V[1][1]) - (V[0][1])*(V[1][0]))/256;
     
-void saveValue(char *str,double p)
- {	
-	FILE *fp;
-	fp = fopen(str,"w");
-	fprintf(fp, "%lf ", p);
-	fclose(fp);
- }
-    
-    //eigen value-a = sqrt(0.5*((V[0][0]+V[1][1])/3-sqrt((a*V[0][1]*V[1][0]) +(V[0][0]-V[1][1])(v[0][0]-V[1][1]))));
-    //eigen value-b = sqrt(0.5*((V[0][0]+V[1][1])/3+sqrt((a*V[0][1]*V[1][0]) +(V[0][0]-V[1][1])(v[0][0]-V[1][1]))));
-    double a = sqrt(V[1][1]/3);
+    double r1 = rootofeqn1(p,q,r);
+    printf("%f\n", r1 );
+
+    double r2 = rootofeqn2(p,q,r);
+    printf("%f\n", r2 );
+
+    double a = sqrt(1/r1);
     saveValue("a.dat",a);
     printf("%f\n\n",a);
     
-    double b = sqrt(V[0][0]/3);
+    double b = sqrt(1/r2);
     saveValue("b.dat",b);
     printf("%f",b);
-
-    theta("theta.dat",10000);
+    
+    double **theta = linspace(0,2*(22/7),100);
+    saveMat(theta,"theta.dat",100,1);
 
 	return 0;
  }	
-	
-double **meshgrid(int len, int begin, double addX, int addY)
-{
-	double ** ret = createMat(len, len);
-	for (int i=0; i<len; i++)
-		for (int j=0; j<len; j++){
-			ret[i][j] = begin + i*addY + j*addX;
-		}
-	return ret;
-}
-
-void saveMat(double** mat, char *str, double m, double n){
-	FILE *fp;
-	fp = fopen(str,"w");
-	for (int i=0; i < m; i++){
-		for (int j=0; j < n; j++){
-			fprintf(fp, "%lf ", mat[i][j]);
-		}
-		fprintf(fp, "\n");
-	}
-	fclose(fp);
-	
-	
-}	
-

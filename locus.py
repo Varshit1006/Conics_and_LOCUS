10b720c11c9e905679f648a19c031d499a386fbf
Varshit1006 <54777494+Varshit1006@users.noreply.github.com>
2019-09-18 15:49:18 +0530

    Update locus.py

--- locus.py
+++ locus.py
@@ -2,23 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 c1 = np.genfromtxt("meshX1.dat")
-c2 = np.genfromtxt("V.dat")
-c3 = np.genfromtxt("u.dat")
-c4 = np.genfromtxt("F.dat")
-c5 = np.genfromtxt("theta.dat")
-c6 = np.genfromtxt("a.dat")
-c7 = np.genfromtxt("b.dat")
-c8 = np.genfromtxt("A.dat")
-c9 = np.genfromtxt("O.dat")
+c2 = np.genfromtxt("A.dat")
+c3 = np.genfromtxt("O.dat")
 
-len = 10000
-y = np.zeros((2,len))
+elips = np.loadtxt('ellipse.dat',dtype='double')
+plt.plot(elips[0,:],elips[1,:],label='ELLIPSE')
 
-y[0,:] = c6*np.cos(c5)
-y[1,:] = c7*np.sin(c5)
-
-plt.plot(y[0,:],y[1,:],label = '$locus$')
-plt.plot(c9,c8)
+plt.plot(c2,c3)
 
 plt.legend()
 plt.axis('equal')
